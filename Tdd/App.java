8fbb1d7d98b9d444c6a406d8e53b3dbc3ed1ec71
Manjot Singh <Manjot Singh>
2025-04-25 14:54:14 +0530

    Manjot [ADD] App

--- Tdd/App.java
+++ Tdd/App.java
@@ -1,4 +1,4 @@
-package org.rituraj;
+package org.manjot;
 
 /**
  * Hello world!
@@ -6,6 +6,7 @@ package org.rituraj;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
